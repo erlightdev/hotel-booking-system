805fb09cbbb8245d0fb386143710b99b522e864f
erlightdev <lightdeveloper@protonmail.com>
2024-12-01 18:33:56 +0545

    resource view created for remaining models

---
+++ app/Filament/Resources/SystemLogResource/Pages/EditSystemLog.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\SystemLogResource\Pages;
+
+use App\Filament\Resources\SystemLogResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditSystemLog extends EditRecord
+{
+    protected static string $resource = SystemLogResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Resources/SystemLogResource/Pages/ViewSystemLog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\SystemLogResource\Pages;
+
+use App\Filament\Resources\SystemLogResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewSystemLog extends ViewRecord
+{
+    protected static string $resource = SystemLogResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+}
